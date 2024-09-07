45ada59504399b9d857711c479be902b6afafc47
Nimira43 <n.rabone72@aol.com>
2024-09-08 00:09:48 +0100

    Paddle now has basic movement

--- scenes/paddle.gd
+++ scenes/paddle.gd
@@ -1,11 +1,13 @@
 extends Area2D
 
+@export var speed: float = 90.0
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_pressed("left"): 
+		position.x += -speed * delta
+		
+	if Input.is_action_pressed("right"): 
+		position.x += speed * delta		
