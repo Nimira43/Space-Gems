c313d4105be111450ea1cc27ff72b84ecdff3422
Nimira43 <n.rabone72@aol.com>
2024-09-08 21:21:03 +0100

    Added signal function to detect when gem hits paddle.

--- scenes/paddle.gd
+++ scenes/paddle.gd
@@ -8,13 +8,17 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	#Paddle Movement
 	#First Option
-	#if Input.is_action_pressed("left"): 
-		#position.x += -speed * delta
-		#
-	#elif Input.is_action_pressed("right"): 
-		#position.x += speed * delta		
+	if Input.is_action_pressed("left"): 
+		position.x += -speed * delta
+		
+	elif Input.is_action_pressed("right"): 
+		position.x += speed * delta		
 	#Second Option
 	#var input_amount: float = Input.get_axis("left", "right")
 	#position.x += delta * speed * input_amount
-	#Third Option 
-	position.x += delta * speed * Input.get_axis("left", "right")
+	#Third Option - preferred but will stick with original code
+	#position.x += delta * speed * Input.get_axis("left", "right")
+
+
+func _on_gem_area_entered(area: Area2D) -> void:
+	print(area)
