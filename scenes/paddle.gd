72a43309f2c516383820c3d0c289616e5a785d44
Nimira43 <n.rabone72@aol.com>
2024-09-08 20:27:48 +0100

    Trying different ways to move paddle.

--- scenes/paddle.gd
+++ scenes/paddle.gd
@@ -1,13 +1,20 @@
 extends Area2D
 
-@export var speed: float = 90.0
+@export var speed: float = 200.0
 
 func _ready() -> void:
 	pass 
 
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("left"): 
-		position.x += -speed * delta
-		
-	if Input.is_action_pressed("right"): 
-		position.x += speed * delta		
+	#Paddle Movement
+	#First Option
+	#if Input.is_action_pressed("left"): 
+		#position.x += -speed * delta
+		#
+	#elif Input.is_action_pressed("right"): 
+		#position.x += speed * delta		
+	#Second Option
+	#var input_amount: float = Input.get_axis("left", "right")
+	#position.x += delta * speed * input_amount
+	#Third Option 
+	position.x += delta * speed * Input.get_axis("left", "right")
